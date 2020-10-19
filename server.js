31abc39dccc37258147885d7e5a0fa165af2776f
Francisco Barron <franz.barron@outlook.com>
2020-10-19 16:06:21 -0500

    Add .env file support

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// Use .env file for environment variables
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Listening on http://localhost:3000');
 });
 
