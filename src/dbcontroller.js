7352e1218dbfa4ce57498283731765c83fadf342
Francisco Barron <franz.barron@outlook.com>
2020-11-11 15:02:42 -0600

    AAAAAA

--- src/dbcontroller.js
+++ src/dbcontroller.js
@@ -1,10 +1,14 @@
 const { Client } = require('pg');
 
-const clientConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-};
+let clientConfig;
+
+if (process.env.PRODUCTION) {
+  clientConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  };
+}
 
 const client = new Client(clientConfig);
 
